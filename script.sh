a06026c291824463418274d7bcbb9c2e33e4c7af
elanozturk <serkanoguzozturk@gmail.com>
2018-01-09 02:31:16 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sshpass -p "Sodomwe255" ssh -q osboxes@192.168.1.13 -oKexAlgorithms=+diffie-hellman-group1-sha1 "/media/sf_xxx/check.sh"
+toilet -f smblock --filter border:metal --filter  metal "elanozturk"
